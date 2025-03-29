5cb2fbf926fe01c149341101a94aad4fb49cd069
lizchu413 <lizchu413@gmail.com>
2025-03-29 12:48:04 -0400

    hello

--- data_parallel/dataset.py
+++ data_parallel/dataset.py
@@ -16,7 +16,7 @@ class Partition():
     def __getitem__(self, index):
         '''Given index, get the data according to the partitioned index'''
         # BEGIN SOLUTION
-        raise NotImplementedError("Data Parallel Not Implemented Yet")
+        return self.data[index]
         # END SOLUTION
 
 # ASSIGNMENT 4.1
@@ -31,16 +31,20 @@ class DataPartitioner():
         2. Create different partitions of indices according to `sizes` and store in `self.partitions`
         '''
         # BEGIN SOLUTION
-        raise NotImplementedError("Data Parallel Not Implemented Yet")
+        indices = list(range(len(self.data)))
+        rng.shuffle(indices)
+        for size in sizes:
+            self.partitions.append(indices[:size])
+            indices = indices[size:]
         # END SOLUTION
 
     def use(self, partition):
-        ''' Return a simple dataset class `Partiton` by original data and partitioned indices
+        ''' Return a simple dataset class `Partition` by original data and partitioned indices
 
         Just one line of code. Think it simply.
         '''
         # BEGIN SOLUTION
-        raise NotImplementedError("Data Parallel Not Implemented Yet")
+        return Partition([self.data[i] for i in self.partitions[partition]], self.partitions[partition])
         # END SOLUTION
 
 # ASSIGNMENT 4.1
@@ -57,5 +61,8 @@ def partition_dataset(rank, world_size, dataset, batch_size=128, collate_fn=None
     4. Wrap the dataset with `DataLoader`, remember to customize the `collate_fn`
     """
     # BEGIN SOLUTION
-    raise NotImplementedError("Data Parallel Not Implemented Yet")
+    part_batch_size = batch_size // world_size
+    part = DataPartitioner(dataset, [1 / world_size] * world_size)
+    curr_part = part.use(rank)
+    return DataLoader(curr_part, batch_size=part_batch_size, shuffle=False, collate_fn=collate_fn)
     # END SOLUTION
