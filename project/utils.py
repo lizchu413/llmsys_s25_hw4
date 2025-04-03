8c8a2d908553a85820cffad2f05a5d43b0fb1a1f
lizchu413 <lizchu413@gmail.com>
2025-04-02 22:56:25 -0400

    hi

--- project/utils.py
+++ project/utils.py
@@ -141,7 +141,8 @@ def train(model, optimizer, examples, batch_size, collate_fn, desc, rank=0, aver
             Just few lines of code. Think simply.
         '''
         # BEGIN SOLUTION
-        average_gradients_fn(model)
+        if average_gradients_fn is not None:
+            average_gradients_fn(model)
         # END SOLUTION
         optimizer.step()
         batch_time = time.time() - t0
